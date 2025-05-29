676132806de99b91eea891c3f0f685e5c56734b0
Lakhdar Chettouh <lakhdar.chettouh2014@gmail.com>
2025-05-29 17:58:06 +0100

    Problem 1

---
+++ ProblemSolving_Level3/3by3_RandomMatrix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int randomNumber(int from, int to)
+{
+	int random = 0;
+	random = rand() % (to - from + 1) + from;
+	return random;
+}
+
+void fillMatrix(int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			arr[i][j] = randomNumber(1, 100);
+	}
+}
+
+void printMatrix(int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << setw(6) << left << arr[i][j];
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	srand((unsigned) time(NULL));
+	int arr[3][3];
+
+	cout << "The following is a 3 * 3 random matrix:\n";
+	fillMatrix(arr);
+	printMatrix(arr);
+
+	return 0;
+}
