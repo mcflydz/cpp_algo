f01f295785ff0278800cf90b435bd5fa17fab718
Lakhdar Chettouh <90472891+mcflydz@users.noreply.github.com>
2025-06-03 23:46:13 +0100

    Problem 44

---
+++ ProblemSolving_Level3/remove_punctuations.cpp
@@ -0,0 +1,29 @@
+/* Write a program to remove all punctuations from string. */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string removePunctuations(string message)
+{
+	string afterRemove = "";
+	for (int i = 0; i < message.length(); i++)
+	{
+		if (!ispunct(message[i]))
+			afterRemove += message[i];
+	}
+	return afterRemove;
+}
+
+int main()
+{
+	string message = "Welcome to Algeria,..; Algeria expresses a warm welcome.";
+	string wordToReplace = "Algeria";
+	string replacement = "USA";
+
+	cout << "Original message:\n" << message << endl;
+	cout << "\nPaunctuations removed:\n" << removePunctuations(message) << endl;
+
+	return 0;
+}
